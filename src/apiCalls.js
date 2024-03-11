74922df0ceffc4b1101bb627b6f89b35767f4d89
Cory Sanders <corysanders3@yahoo.com>
2024-03-11 14:27:00 -0600

    change urls to deployed servers

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,22 +1,22 @@
 function fetchUserData(usersData) {
-  return fetch("http://localhost:3001/api/v1/users")
+  return fetch("https://fitlit-server-18177a023c25.herokuapp.com/api/v1/users")
     .then((response) => response.json())
     .then((data) => (usersData = data));
 }
 function fetchHydrationData(hydration) {
-  return fetch("http://localhost:3001/api/v1/hydration")
+  return fetch("https://fitlit-server-18177a023c25.herokuapp.com/api/v1/hydration")
     .then((repsonse) => repsonse.json())
     .then((data) => (hydration = data));
 }
 
 function fetchSleepData(sleep) {
-  return fetch("http://localhost:3001/api/v1/sleep")
+  return fetch("https://fitlit-server-18177a023c25.herokuapp.com/api/v1/sleep")
     .then((repsonse) => repsonse.json())
     .then((data) => (sleep = data));
 }
 
 function postHydrationData(date, numOunces, userID) {
-  fetch("http://localhost:3001/api/v1/hydration", {
+  fetch("https://fitlit-server-18177a023c25.herokuapp.com/api/v1/hydration", {
     method: 'POST',
     body: JSON.stringify({
       date,
